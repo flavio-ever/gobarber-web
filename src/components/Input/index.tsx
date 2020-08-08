ca935c4462fb46edffeea56e549a8d6f9a528145
Flavio Ever <flavio.ever@outlook.com>
2020-08-07 23:33:31 -0300

    handleInputFocus

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -37,11 +37,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
-        onFocus={() => setIsFocused(true)}
+        onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
